3b91d914eb32b3544e54491bf0474b62e7ee2beb
vanitha2575 <167955018+vanitha2575@users.noreply.github.com>
2024-04-28 19:51:26 +0530

    p3

---
+++ p3.py
@@ -0,0 +1,10 @@
+def check_weiredness(n):
+   if n% 2!=0:
+     print("weird")
+   else:
+      if n in range(2,6):
+         print("Not weird")
+      elif in range(6,21):
+         print("weird")
+      else:
+         print("Not weird")
